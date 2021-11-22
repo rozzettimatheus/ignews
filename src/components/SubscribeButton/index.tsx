43253821c52a8fe69f0d3dd6755ecd75014fcad2
Gabriel Rozzetti <rozzetti.matheus@gmail.com>
2021-11-22 11:45:49 -0300

    add stripe config and payment method through application

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -19,6 +19,7 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
     try {
       const response = await api.post('/subscribe')
+
       const { sessionId } = response.data
 
       const stripe = await getStripeJs()
---
+++ src/pages/api/_lib/handleSubscription.ts
@@ -0,0 +1,50 @@
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+import { stripe } from '../../../services/stripe'
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  createAction = false
+) {
+  const userRef = await fauna.query(
+    q.Select('ref', q.Get(q.Match(q.Index('user_by_customer_id'), customerId)))
+  )
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef, // fauna ref
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  }
+
+  if (createAction) {
+    // add is-case if subscription by stripe is allowed
+    await fauna.query(
+      q.Create(q.Collection('subscriptions'), {
+        data: subscriptionData,
+      })
+    )
+  } else {
+    try {
+      console.log('aqui')
+
+      await fauna.query(
+        // q.Update(..., data: { status: subscriptionData.status })
+        q.Replace(
+          q.Select(
+            'ref',
+            q.Get(q.Match(q.Index('subscription_by_id'), subscriptionId))
+          ),
+          {
+            data: subscriptionData,
+          }
+        )
+      )
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
